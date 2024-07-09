a7833ad52e902a21516136d4c6a4aa3bbe357184
GrayJunZi <GrayJunZi@outlook.com>
2024-07-10 07:28:41 +0800

    28. Ringston JavaScript Setup

--- Final/README.md
+++ Final/README.md
@@ -9,6 +9,6 @@ npm create vite@latest
 
 ## 2. 安装依赖
 ```bash
-npm i gsap three @studio-freight/lenis split-type
+npm i gsap three @studio-freight/lenis split-type dat.gui
 ```
 
--- Final/ringston/main.js
+++ Final/ringston/main.js
@@ -1,1 +1,68 @@
-import './style.css'
+import "./style.css";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { TextPlugin } from "gsap/TextPlugin";
+import { GLTFLoading } from "three/addons/loaders/GLTFLoader.js";
+import SplitType from "split-type";
+import * as dat from "dat.gui";
+import * as THREE from "three";
+import Lenis from "lenis";
+import gsap from "gsap";
+
+gsap.registerPlugin(TextPlugin);
+gsap.registerPlugin(ScrollTrigger);
+
+function initThreeJS() {
+  // Debug
+  const gui = new dat.GUI();
+  gui.toggleHide();
+
+  // Canvas
+  const canvas = document.querySelector("canvas.webgl");
+
+  // Scene
+  const scene = new THREE.Scene();
+
+  const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight,
+  };
+
+  // Camera
+  const camera = new THREE.PerspectiveCamera(
+    75,
+    sizes.width / sizes.height,
+    0.1,
+    100
+  );
+  camera.position.x = 0;
+  camera.position.y = 0;
+  camera.position.z = 2;
+  scene.add(camera);
+
+  // Renderer
+  const renderer = new THREE.WebGLRenderer({
+    canvas: camera,
+    alpha: true,
+    antialias: true,
+  });
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+  window.addEventListener("resize", () => {
+    // Update sizes
+    sizes.width = window.innerWidth;
+    sizes.height = window.innerHeight;
+
+    // Update camera
+    camera.aspect = sizes.width / sizes.height;
+    camera.updateProjectionMatrix();
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height);
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+  });
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  initThreeJS();
+});
